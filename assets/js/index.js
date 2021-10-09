28cefbbb80fbd1a8ba8855877a77f3c0c9c8c6e1
AkenEagle <akeneagle@gmail.com>
2021-10-09 19:40:27 +0100

    Added event on click

---
+++ assets/js/index.js
@@ -0,0 +1,10 @@
+// Get start button
+const btnStart = document.getElementById("btn-start");
+
+// Function for the start button
+const startQuiz = function () {
+    console.log("Quiz started");
+}
+
+// Assign function to the button click event
+btnStart.addEventListener("click", startQuiz);
