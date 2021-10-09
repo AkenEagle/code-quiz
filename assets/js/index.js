649c06e88ee8b7e500751e6947838b20b3d205eb
AkenEagle <akeneagle@gmail.com>
2021-10-09 20:39:09 +0100

    Load question

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,27 @@
 // Get elements
 const btnStart = document.getElementById("btn-start");
 const timeValue = document.getElementById("time-value");
+const divQuiz = document.getElementById("quiz-box");
+
+// To handle progress
+let quizStep = 0;
+
+// Questions
+let questions = [
+    "1. We use _____ to link a CSS file to HTML",
+    "2. The default value of a variable is ______"
+]
+
+// Answers
+let answers = [
+    [
+        "<script>", "<link>", "<html>", "<css>"
+    ],
+
+    [
+        "undefined", "true", "256", "1"
+    ]
+]
 
 // Timer
 let timer;
@@ -16,12 +37,34 @@ const startTimer = function () {
     }
 }
 
+// Function to render a question
+const renderQuestion = function () {
+    divQuiz.innerHTML = "";
+
+    // Load question
+    const h2 = document.createElement("h2");
+    const question = document.createTextNode(questions[quizStep]);
+    h2.appendChild(question);
+    divQuiz.appendChild(h2);
+
+    // // Load answers
+    // for (let i = 0; i < 4; i++) {
+    //     const btn = document.createElement("div");
+    //     btn.setAttribute("class", "btn");
+    //     const answer = document.createTextNode(answers[quizStep][i]);
+    //     btn.appendChild(answer);
+    //     divQuiz.appendChild(btn);
+    // }
+
+}
+
 // Function for the start button
 const startQuiz = function () {
     // Start the timer
     timer = setInterval(startTimer, 1000);
 
     // Render first question
+    renderQuestion();
 }
 
 // Assign function to the button click event
