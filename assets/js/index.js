dfbaab4be79c58090c9c41e171dd1fdd6ba24522
AkenEagle <akeneagle@gmail.com>
2021-10-09 22:07:39 +0100

    Handle answers and render next question

--- assets/js/index.js
+++ assets/js/index.js
@@ -23,39 +23,97 @@ let answers = [
     ]
 ]
 
+// Correct answer number
+let correctAnswers = [
+    "<link>",
+    "undefined"
+]
+
 // Timer
 let timer;
 let allowedTime = 3; // How many seconds will the timer be set
 
 // Timer function
 const startTimer = function () {
+    timeValue.textContent = allowedTime;
     if (allowedTime === 0) {
         clearInterval(startTimer);
+
     } else {
         allowedTime -= 1;
-        timeValue.textContent = allowedTime;
+    }
+}
+
+// Function to display answer feedback
+const answerFeedback = function (text) {
+
+}
+
+// Function to render next question
+const renderNextQuestion = function () {
+    const question = document.getElementById("question");
+    question.textContent = questions[quizStep];
+
+    for (let i = 0; i < 4; i++) {
+        const answer = document.getElementById("btn-answer-"+i);
+        answer.textContent = answers[quizStep][i];
+    }
+}
+
+// Function to render when the questions finish
+const renderFinished = function () {
+    
+}
+
+// Function to handle an answer
+const handleAnswer = function () {
+    const clickedAnswer = this.textContent;
+    let feedbackAnswer;
+    if (correctAnswers.includes(clickedAnswer)) {
+        feedbackAnswer = "Correct!"
+    } else {
+        feedbackAnswer = "Wrong!"
+    }
+
+    // Display feedback
+    const divFeedback = document.createElement("div");
+    divFeedback.setAttribute("class", "answer-feedback")
+    const feedbackText = document.createTextNode(feedbackAnswer);
+    divFeedback.appendChild(feedbackText);
+    divQuiz.appendChild(divFeedback);
+    setTimeout(function() {
+        divQuiz.removeChild(divFeedback);
+    }, 1000);
+
+    quizStep += 1;
+    if(quizStep == questions.length) {
+        renderFinished();
+    } else {
+        renderNextQuestion();
     }
 }
 
 // Function to render a question
-const renderQuestion = function () {
+const renderFirstQuestion = function () {
     divQuiz.innerHTML = "";
 
     // Load question
     const h2 = document.createElement("h2");
+    h2.setAttribute("id", "question");
     const question = document.createTextNode(questions[quizStep]);
     h2.appendChild(question);
     divQuiz.appendChild(h2);
 
     // Load answers
     for (let i = 0; i < 4; i++) {
-        const btn = document.createElement("div");
+        btn = document.createElement("div");
         btn.setAttribute("class", "btn-answer");
+        btn.setAttribute("id", "btn-answer-"+i)
         const answer = document.createTextNode(answers[quizStep][i]);
         btn.appendChild(answer);
         divQuiz.appendChild(btn);
+        btn.addEventListener("click", handleAnswer);
     }
-
 }
 
 // Function for the start button
@@ -64,7 +122,7 @@ const startQuiz = function () {
     timer = setInterval(startTimer, 1000);
 
     // Render first question
-    renderQuestion();
+    renderFirstQuestion();
 }
 
 // Assign function to the button click event
