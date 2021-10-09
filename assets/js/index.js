c4a221ca2d10f4377ab2aefa0c0defd3af8241c7
AkenEagle <akeneagle@gmail.com>
2021-10-09 19:51:59 +0100

    Timer function

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,27 @@
-// Get start button
+// Get elements
 const btnStart = document.getElementById("btn-start");
+const timeValue = document.getElementById("time-value");
+
+// Timer
+let timer;
+let allowedTime = 3; // How many seconds will the timer be set
+
+// Timer function
+const startTimer = function () {
+    if (allowedTime === 0) {
+        clearInterval(startTimer);
+    } else {
+        allowedTime -= 1;
+        timeValue.textContent = allowedTime;
+    }
+}
 
 // Function for the start button
 const startQuiz = function () {
-    console.log("Quiz started");
+    // Start the timer
+    timer = setInterval(startTimer, 1000);
+
+    // Render first question
 }
 
 // Assign function to the button click event
