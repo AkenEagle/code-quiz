a5a14549faecf45bc20f4e7153a6ba51e1c53369
AkenEagle <akeneagle@gmail.com>
2021-10-16 23:25:27 +0100

    Test new location for GitHub pages

--- assets/js/index.js
+++ assets/js/index.js
@@ -48,7 +48,7 @@ const renderGameOver = function () {
     btnGoBack.setAttribute("class", "btn-start btn-goback");
 
     const link = document.createElement("a");
-    link.setAttribute("href", "/");
+    link.setAttribute("href", "./");
     link.innerText = "Go Back";
 
     btnGoBack.append(link);
